261581ed676ae9c9017cc8863b320233b1c4ed6d
Saida Mahmood <saida.mahmood04@gmail.com>
2022-02-07 20:01:23 +0000

    courses page added

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import HomePage from '../components/HomePage';
 import AboutPage from '../components/AboutPage';
 import Header from './common/Header';
+import CoursePage from './CoursePage';
 
 
 function App(){
     function getPage(){
 
     const route = window.location.pathname;
+    if(route === "/courses") return <CoursePage />;
     if(route === "/about") return <AboutPage />;
     return <HomePage/>
     }

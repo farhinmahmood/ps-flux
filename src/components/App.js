cfeb6f455270dabb0649ecf77f652c7eceba5c2f
Saida Mahmood <saida.mahmood04@gmail.com>
2022-03-01 12:30:37 +0000

    redirect

--- src/components/App.js
+++ src/components/App.js
@@ -3,20 +3,22 @@ import HomePage from '../components/HomePage';
 import AboutPage from '../components/AboutPage';
 import Header from './common/Header';
 import CoursePage from './CoursePage';
+import {Route,Switch,Redirect } from 'react-router-dom';
+import NotFound from './NotFound';
 
 
 function App(){
-    function getPage(){
-
-    const route = window.location.pathname;
-    if(route === "/courses") return <CoursePage />;
-    if(route === "/about") return <AboutPage />;
-    return <HomePage/>
-    }
     return(
         <div className='container-fluid'>
             <Header/>
-            {getPage()}
+            <Switch>
+                <Route path="/" exact component={HomePage}/>
+                <Route path="/courses" component={CoursePage}/>
+                <Route path="/about" component={AboutPage}/>
+                <Redirect from="/about-page" to="about"/>
+                <Route component={NotFound}/>
+            </Switch>
+            
         </div>
     );
 
